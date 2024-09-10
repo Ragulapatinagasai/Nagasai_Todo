457d6dd530097f0a7d1f19210c39f00fdb8d4b30
Naga sai <sainaga805@gmail.com>
2024-09-10 18:09:57 +0530

    hello

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/", require("./routes/authrouts"));
 
-const CONNECTION_URL = "mongodb://0.0.0.0:27017/";
+// const CONNECTION_URL = "mongodb://0.0.0.0:27017/";
+const CONNECTION_URL="mongodb+srv://sainaga805:0CAFOSymSCKkuWJc@cluster0.babz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const PORT = 8800;
 
